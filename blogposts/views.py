53be392cc25e3827798b2e3b679f83dea6d18e1d
shivavemula5 <115597456+shivavemula5@users.noreply.github.com>
2023-05-12 15:49:51 +0530

    home page updated twice

--- blogposts/views.py
+++ blogposts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.db.models import OuterRef , Subquery , Prefetch
 from django.db import connection
@@ -7,7 +8,7 @@ from accounts.models import UserAccount
 from blogposts.models import Post , Comment , SavedPosts , LikedPosts 
 
 def home(request):
-    return render(request, 'temp.html',{'message':'hello'})
+    return HttpResponse("hii")
 
 def practiseQuery(request):
     pass
